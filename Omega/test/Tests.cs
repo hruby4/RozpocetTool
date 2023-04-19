8aa8c745031a5106065e471cf7d76d10fda7f500
hruby4 <jakub.hruby@centrum.cz>
2023-04-19 10:27:24 +0200

    documentation

---
+++ Omega/test/Tests.cs
@@ -0,0 +1,146 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega.test
+{
+    [TestFixture]
+    public class Tests
+    {
+        //one_nonDigit_Word
+        [Test]
+        public void one_nonDigit_TestValidWord()
+        {
+            // Arrange
+            string word = "hello";
+
+            // Act
+            bool result = RegexValidator.one_nonDigit_Word(word);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void one_nonDigit_TestInvalidWordWithDigits()
+        {
+            // Arrange
+            string word = "hello123";
+
+            // Act
+            bool result = RegexValidator.one_nonDigit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_nonDigit_TestInvalidWordWithSpecialCharacters()
+        {
+            // Arrange
+            string word = "hello!";
+
+            // Act
+            bool result = RegexValidator.one_nonDigit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_nonDigit_TestEmptyString()
+        {
+            // Arrange
+            string word = "";
+
+            // Act
+            bool result = RegexValidator.one_nonDigit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_nonDigit_Word_TestNullString()
+        {
+            // Arrange
+            string word = null;
+
+            // Act
+            bool result = RegexValidator.one_nonDigit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        //one_digit_Word
+
+        [Test]
+        public void one_digit_Word_TestValidWord()
+        { 
+            // Arrange
+            string word = "hello123";
+
+            // Act
+            bool result = RegexValidator.one_digit_Word(word);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void one_digit_Word_TestInvalidWordWithoutDigits()
+        {
+            // Arrange
+            string word = "hello";
+
+            // Act
+            bool result = RegexValidator.one_digit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_digit_Word_TestInvalidWordWithSpecialCharacters()
+        {
+            // Arrange
+            string word = "hello!";
+
+            // Act
+            bool result = RegexValidator.one_digit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_digit_Word_TestEmptyString()
+        {
+            // Arrange
+            string word = "";
+
+            // Act
+            bool result = RegexValidator.one_digit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void one_digit_Word_TestNullString()
+        {
+            // Arrange
+            string word = null;
+
+            // Act
+            bool result = RegexValidator.one_digit_Word(word);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
